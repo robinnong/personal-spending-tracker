55a21866e7698aec898d47e03733355ecac0dc80
robinnong <robin.nong@hotmail.com>
2020-08-24 00:14:17 -0400

    Refactored sorting action

--- src/redux/reducers/sorting.js
+++ src/redux/reducers/sorting.js
@@ -2,14 +2,9 @@
 
   switch (action.type) {
     case 'SORT_COLUMN':  
-    if (action.text === "name") {
-      return { column: action.text, direction: true } 
-    } else if (action.text === "category" || action.text === "date") {
-      return { column: action.text, direction: false } 
-    } else {
-      return { column: action.text, direction: true } 
-
-    }
+      return (action.text === "category" || action.text === "date") 
+        ? { column: action.text, direction: false }  
+        : { column: action.text, direction: true } 
     default:
       return state
   }
